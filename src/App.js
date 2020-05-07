7c33fdd4bb2520d6f43b036864c3951dc778e2f3
Tejkumar3 <achanta.kumar@brillio.com>
2020-05-07 19:49:35 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import './App.css';
-import './global.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import SignupPage from './Profile/Components/Signup/SignupPage';
-import LoginPage from './Profile/Components/Login/LoginPage';
-import HomePage from './Home/HomePage';
+import React from "react";
+import "./App.css";
+import "./global.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SignupPage from "./Profile/Components/Signup/SignupPage";
+import LoginPage from "./Profile/Components/Login/LoginPage";
+import HomePage from "./Home/HomePage";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route component={HomePage} path="/" exact />
-        <Route component={LoginPage} path='/login' exact />
-        <Route component={SignupPage} path='/signup' exact />
+        <Switch>
+          <Route component={HomePage} path="/" exact />
+          <Route component={LoginPage} path="/login" exact />
+          <Route component={SignupPage} path="/signup" exact />
+        </Switch>
       </Router>
     </div>
   );
