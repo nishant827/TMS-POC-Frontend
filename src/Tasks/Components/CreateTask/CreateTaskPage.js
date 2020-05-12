8e20c197dbf177ab60e51e0a2c624fb20ec33c27
rgambhire <64951724+rgambhire@users.noreply.github.com>
2020-05-12 00:21:40 -0700

    fixed select

--- src/Tasks/Components/CreateTask/CreateTaskPage.js
+++ src/Tasks/Components/CreateTask/CreateTaskPage.js
@@ -2,6 +2,7 @@ import React, { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
 import { Row, Col } from "react-bootstrap";
 import Alert from "react-bootstrap/Alert";
+import Select from 'react-select';
 
 const CreateTaskPage = () => {
   const [formData, setFormData] = useState({
@@ -10,13 +11,17 @@ const CreateTaskPage = () => {
     taskType: "",
     taskTitle: "",
     taskDescription: "",
-    technicianName: "",
+    technicianName: null,
+    technicians: [
+      { value: 'sam', label:'Sam' },
+      { value: 'Smith', label: 'Smith'},
+      {value: 'Karen', label: 'Karen'}],
     startDate: "",
     estimatedEndDate: "",
     status: "",
   });
 
-  const { towerId, address, taskType, taskTitle, taskDescription, technicianName, startDate, estimatedEndDate, status } = formData;
+  const { towerId, address, taskType, taskTitle, taskDescription, technicianName, technicians,startDate, estimatedEndDate, status } = formData;
 
   const onChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -27,130 +32,136 @@ const CreateTaskPage = () => {
       console.log( towerId, address, taskType, taskTitle, taskDescription, technicianName, startDate, estimatedEndDate, status);
       alert("Task created successfully!!");
   };
-
+  const handleTechnicianSelectChange = e => {
+    setFormData({ ...formData, [technicianName]: e[0].value });
+  };
   return (
     <Fragment>
       <h1 className="large text-primary">Create Task</h1>
       <form className="form" onSubmit={(e) => onSubmit(e)}>
-      <div className= "row" >
-        <div className= "col-sm-4">
-          <div className="form-group">
-            <input
-              type="text"
-              className= "form-control"
-              placeholder="TowerId"
-              name="towerId"
-              value={ towerId}
-              onChange={(e) => onChange(e)}
-            />
-          </div>
-        </div>
-        <div className= "col-sm-4">
-          <div className="form-group">
-            <input
-              type="text"
-              className= "form-control"
-              placeholder="Site address"
-              name="address"
-              value={address}
-              onChange={(e) => onChange(e)}
-            />
-          </div>
-        </div>
-        
-      </div>  
       <div className="row">
-        <div className= "col-sm-4">
-          <div className="form-group">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="task type"
-              name="taskType"
-              value={taskType}
-              onChange={(e) => onChange(e)}
-            />
+        <div className="col-sm-2"></div>
+        <div className="col-sm-8">
+          <div className= "row" >
+            <div className= "col-sm-6">
+              <div className="form-group">
+                <input
+                  type="text"
+                  className= "form-control"
+                  placeholder="TowerId"
+                  name="towerId"
+                  value={ towerId}
+                  onChange={(e) => onChange(e)}
+                />
+              </div>
+            </div>
+            <div className= "col-sm-6">
+              <div className="form-group">
+                <input
+                  type="text"
+                  className= "form-control"
+                  placeholder="Site address"
+                  name="address"
+                  value={address}
+                  onChange={(e) => onChange(e)}
+                />
+              </div>
+            </div>
+            
+          </div>  
+          <div className="row">
+            <div className= "col-sm-6">
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="task type"
+                  name="taskType"
+                  value={taskType}
+                  onChange={(e) => onChange(e)}
+                />
+              </div>
+            </div>
+            <div className= "col-sm-6">
+            <div className="form-group">
+              <input
+                type="text"
+                className= "form-control input-lg"
+                placeholder="task title"
+                name="taskTitle"
+                value={taskTitle}
+                onChange={(e) => onChange(e)}
+              />
+            </div>
+            </div>
           </div>
-        </div>
-        <div className= "col-sm-4">
-        <div className="form-group">
-          <input
-            type="text"
-            className= "form-control input-lg"
-            placeholder="task title"
-            name="taskTitle"
-            value={taskTitle}
-            onChange={(e) => onChange(e)}
-          />
-        </div>
-        </div>
-      </div>
-      <div className="row">
-        <div className= "col-sm-4">
-          <div className="form-group">
-            <input
-              type="text"
-              className= "form-control input-lg"
-              placeholder="task description"
-              name="taskDescription"
-              value={taskDescription}
-              onChange={(e) => onChange(e)}
-            />
+          <div className="row">
+            <div className= "col-sm-6">
+              <div className="form-group">
+                <input
+                  type="text"
+                  className= "form-control input-lg"
+                  placeholder="task description"
+                  name="taskDescription"
+                  value={taskDescription}
+                  onChange={(e) => onChange(e)}
+                />
+              </div>
+            </div>
+            <div className="col-sm-6">
+              <div className="form-group">
+                <Select
+                  name="technicians"
+                  placeholder="select Technician"
+                  onChange={(e)=>handleTechnicianSelectChange(e)}
+                  options={technicians}
+                  isMulti= {true}
+                />
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="col-sm-4">
-          <div className="form-group">
-            <input
-              type="text"
-              className= "form-control input-lg"
-              placeholder="technician name"
-              name="technicianName"
-              value={technicianName}
-              onChange={(e) => onChange(e)}
-            />
+          <div className="row">
+            <div className="col-sm-6">
+              <div className="form-group">
+                <input
+                  type="text"
+                  className= "form-control input-lg"
+                  placeholder="start date"
+                  name="startDate"
+                  value={startDate}
+                  onChange={(e) => onChange(e)}
+                />
+              </div>
+            </div>
+            <div className="col-sm-6">
+              <div className="form-group">
+                <input
+                  type="text"
+                  className= "form-control input-lg"
+                  placeholder="estimated End Date"
+                  name="estimatedEndDate"
+                  value={estimatedEndDate}
+                  onChange={(e) => onChange(e)}
+                />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-sm-4">
-          <div className="form-group">
-            <input
-              type="text"
-              className= "form-control input-lg"
-              placeholder="start date"
-              name="startDate"
-              value={startDate}
-              onChange={(e) => onChange(e)}
-            />
+          <div className="row">
+            <div className="col-sm-6">
+              <div className="form-group">
+                <input
+                  type="text"
+                  className= "form-control input-lg"
+                  placeholder="status"
+                  name="status"
+                  value={status}
+                  onChange={(e) => onChange(e)}
+                />
+              </div>
+            </div>
           </div>
+        </div>  
         </div>
-        <div className="col-sm-4">
-          <div className="form-group">
-            <input
-              type="text"
-              className= "form-control input-lg"
-              placeholder="estimated End Date"
-              name="estimatedEndDate"
-              value={estimatedEndDate}
-              onChange={(e) => onChange(e)}
-            />
-          </div>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-sm-4">
-          <div className="form-group">
-            <input
-              type="text"
-              className= "form-control input-lg"
-              placeholder="status"
-              name="status"
-              value={status}
-              onChange={(e) => onChange(e)}
-            />
-          </div>
-        </div>
-      </div>
         <input type="submit" className="primary btn btn-primary" value="Create Task" />
       </form>
       
--- src/Tasks/Components/UpdateTask/UpdateTaskPage.js
+++ src/Tasks/Components/UpdateTask/UpdateTaskPage.js
@@ -37,11 +37,15 @@ const UpdateTaskPage = (props) => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-      alert("Task created successfully!!");
+    console.log(e);
+      alert("Task Updated successfully!!");
   };
   
   const handleTechnicianSelectChange = e => {
+    console.log(technicianName);
+    console.log(e, e[0]);
     setFormData({ ...formData, [technicianName]: e[0].value });
+    console.log(technicianName)
   };
   const handleItemSelectChange = e => {
     setFormData({ ...formData, [item]: e.value });
@@ -51,134 +55,139 @@ const UpdateTaskPage = (props) => {
     <Fragment>
       <h1 className="large text-primary">Update Task</h1>
       <form className="form" onSubmit={(e) => onSubmit(e)}>
-      <div className= "row" >
-        <div className= "col-sm-4">
-          <div className="form-group">
-            <input
-              type="text"
-              className= "form-control"
-              placeholder="TowerId"
-              name="towerId"
-              value={ towerId}
-              onChange={(e) => onChange(e)}
-            />
+        <div className= "row">
+        <div className="col-sm-2"></div>
+        <div className="col-sm-8">
+          <div className= "row" >
+            <div className= "col-sm-6">
+              <div className="form-group">
+                <input
+                  type="text"
+                  className= "form-control"
+                  placeholder="TowerId"
+                  name="towerId"
+                  value={ towerId}
+                  onChange={(e) => onChange(e)}
+                />
+              </div>
+            </div>
+            <div className= "col-sm-6">
+              <div className="form-group">
+                <input
+                  type="text"
+                  className= "form-control"
+                  placeholder="Site address"
+                  name="address"
+                  value={address}
+                  onChange={(e) => onChange(e)}
+                />
+              </div>
+            </div>
+            
+          </div>  
+          <div className="row">
+            <div className= "col-sm-6">
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="task type"
+                  name="taskType"
+                  value={taskType}
+                  onChange={(e) => onChange(e)}
+                />
+              </div>
+            </div>
+            <div className= "col-sm-6">
+            <div className="form-group">
+              <input
+                type="text"
+                className= "form-control input-lg"
+                placeholder="task title"
+                name="taskTitle"
+                value={taskTitle}
+                onChange={(e) => onChange(e)}
+              />
+            </div>
+            </div>
           </div>
-        </div>
-        <div className= "col-sm-4">
-          <div className="form-group">
-            <input
-              type="text"
-              className= "form-control"
-              placeholder="Site address"
-              name="address"
-              value={address}
-              onChange={(e) => onChange(e)}
-            />
+          <div className="row">
+            <div className= "col-sm-6">
+              <div className="form-group">
+                <input
+                  type="text"
+                  className= "form-control input-lg"
+                  placeholder="task description"
+                  name="taskDescription"
+                  value={taskDescription}
+                  onChange={(e) => onChange(e)}
+                />
+              </div>
+            </div>
+            <div className="col-sm-6">
+              <div className="form-group">
+              <Select
+                name="technicians"
+                placeholder="select Technician"
+                onChange={(e)=>handleTechnicianSelectChange(e)}
+                options={technicians}
+                isMulti
+              />
+              </div>
+            </div>
           </div>
-        </div>
-        
-      </div>  
-      <div className="row">
-        <div className= "col-sm-4">
-          <div className="form-group">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="task type"
-              name="taskType"
-              value={taskType}
-              onChange={(e) => onChange(e)}
-            />
+          <div className="row">
+            <div className="col-sm-6">
+              <div className="form-group">
+                <input
+                  type="text"
+                  className= "form-control input-lg"
+                  placeholder="start date"
+                  name="startDate"
+                  value={startDate}
+                  onChange={(e) => onChange(e)}
+                />
+              </div>
+            </div>
+            <div className="col-sm-6">
+              <div className="form-group">
+                <input
+                  type="text"
+                  className= "form-control input-lg"
+                  placeholder="estimated End Date"
+                  name="estimatedEndDate"
+                  value={estimatedEndDate}
+                  onChange={(e) => onChange(e)}
+                />
+              </div>
+            </div>
           </div>
-        </div>
-        <div className= "col-sm-4">
-        <div className="form-group">
-          <input
-            type="text"
-            className= "form-control input-lg"
-            placeholder="task title"
-            name="taskTitle"
-            value={taskTitle}
-            onChange={(e) => onChange(e)}
-          />
-        </div>
-        </div>
-      </div>
-      <div className="row">
-        <div className= "col-sm-4">
-          <div className="form-group">
-            <input
-              type="text"
-              className= "form-control input-lg"
-              placeholder="task description"
-              name="taskDescription"
-              value={taskDescription}
-              onChange={(e) => onChange(e)}
-            />
+          <div className="row">
+            <div className="col-sm-6">
+              <div className="form-group">
+                <input
+                  type="text"
+                  className= "form-control input-lg"
+                  placeholder="status"
+                  name="status"
+                  value={status}
+                  onChange={(e) => onChange(e)}
+                />
+              </div>
+            </div>
+            <div className="col-sm-6">
+              <Select
+                placeholder="select item"
+                onChange={(e)=>handleItemSelectChange(e)}
+                options={options}
+                isMulti
+              />
+            </div>
           </div>
-        </div>
-        <div className="col-sm-4">
-          <div className="form-group">
-          <Select
-            name="technicians"
-            placeholder="select Technician"
-            value={item}
-            onChange={(e)=>handleTechnicianSelectChange(e)}
-            options={technicians}
-            isMulti= {true}
-          />
-          </div>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-sm-4">
-          <div className="form-group">
-            <input
-              type="text"
-              className= "form-control input-lg"
-              placeholder="start date"
-              name="startDate"
-              value={startDate}
-              onChange={(e) => onChange(e)}
-            />
-          </div>
-        </div>
-        <div className="col-sm-4">
-          <div className="form-group">
-            <input
-              type="text"
-              className= "form-control input-lg"
-              placeholder="estimated End Date"
-              name="estimatedEndDate"
-              value={estimatedEndDate}
-              onChange={(e) => onChange(e)}
-            />
-          </div>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-sm-4">
-          <div className="form-group">
-            <input
-              type="text"
-              className= "form-control input-lg"
-              placeholder="status"
-              name="status"
-              value={status}
-              onChange={(e) => onChange(e)}
-            />
-          </div>
-        </div>
-        <div className="col-sm-4">
-          <Select
-            placeholder="select item"
-            value={item}
-            onChange={(e)=>handleItemSelectChange(e)}
-            options={options}
-          />
-        </div>
-      </div>
         <input type="submit" className="primary btn btn-primary" value="Update Task" />
+      </div>
+      <div className="col-sm-2"></div>  
+      </div>  
       </form>
     </Fragment>
   );
